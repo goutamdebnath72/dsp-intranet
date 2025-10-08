b0ce9cb701b70cc88ff02108e5b977ec2d5aa8c9
Goutam Debnath <goutam.debnath72@gmail.com>
2025-10-08 07:18:14 +0000

    fix: Returning back to next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const config: NextConfig = {
+  // Add the 'images' configuration here
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default config;
